99d2aaa6542817139712def2d258df931850896e
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-02 08:21:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const buttons = document.querySelectorAll(".drum");
+// const buttons = document.querySelectorAll(".drum");
+// function handleClick(){
+//     alert("I got clicked!")
+// }
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", handleClick)});
+
+let buttons = document.querySelectorAll(".drum").length;
 function handleClick(){
-    alert(" I got clicked!")
+        alert("I got clicked!")
+    }
+for(let i=0; i<buttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 }
-buttons.forEach(function(button) {
-    button.addEventListener("click", handleClick)})
+
+
 // document.querySelector("").addEventListener("click",handleClick);
