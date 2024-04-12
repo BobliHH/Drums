7638c6f743b9783e87d5d9cdf5f893e37f7b5ae3
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-12 09:28:31 +0300

    formatted text

--- index.js
+++ index.js
@@ -8,64 +8,60 @@
 //DETECTING BUTTON PRESS
 let buttons = document.querySelectorAll(".drum").length;
 
-function handleClick(){ 
-
-    let buttonInnerHTML =this.innerHTML;
-    handleKey(buttonInnerHTML);}
-
-for(let i=0; i<buttons; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+function handleClick() {
+  let buttonInnerHTML = this.innerHTML;
+  handleKey(buttonInnerHTML);
 }
 
+for (let i = 0; i < buttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
 
 // DETECTING KEY PRESS
-document.addEventListener("keydown",function(event){
-    
-    console.log(event)
-    handleKey(event.key)
-    
-})
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+  handleKey(event.key);
+});
 
-function handleKey(key){
+function handleKey(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio(`./sounds/tom-1.mp3`);
+      tom1.play();
+      break;
 
-   
-    switch (key) {
-        case "w":
-            let tom1 = new Audio(`./sounds/tom-1.mp3`);
-            tom1.play();
-            break;
+    case "a":
+      let tom2 = new Audio(`./sounds/tom-2.mp3`);
+      tom2.play();
+      break;
 
-        case "a":
-        let tom2 = new Audio(`./sounds/tom-2.mp3`);
-        tom2.play();
-            break;
-        
-            case "s":
-                let tom3 = new Audio(`./sounds/tom-3.mp3`);
-                tom3.play();
-                    break;    
+    case "s":
+      let tom3 = new Audio(`./sounds/tom-3.mp3`);
+      tom3.play();
+      break;
 
-                    case "d":
-                        let tom4 = new Audio(`./sounds/tom-4.mp3`);
-                        tom4.play();
-                            break;  
+    case "d":
+      let tom4 = new Audio(`./sounds/tom-4.mp3`);
+      tom4.play();
+      break;
 
-                            case "j":
-        let snare = new Audio(`./sounds/snare.mp3`);
-        snare.play();
-            break;   
-            
-            case "k":
-        let crash = new Audio(`./sounds/crash.mp3`);
-        crash.play();
-            break;
+    case "j":
+      let snare = new Audio(`./sounds/snare.mp3`);
+      snare.play();
+      break;
 
-            case "l":
-        let kick = new Audio(`./sounds/kick-bass.mp3`);
-        kick.play();
-            break;
+    case "k":
+      let crash = new Audio(`./sounds/crash.mp3`);
+      crash.play();
+      break;
 
-    
-        default: console.log("buttonInnerHTML")
-            break;
-    }}
+    case "l":
+      let kick = new Audio(`./sounds/kick-bass.mp3`);
+      kick.play();
+      break;
+
+    default:
+      console.log("buttonInnerHTML");
+      break;
+  }
+}
